7c8b384b361ec523b332e5572363acde0d29b9df
juan <juancastrillon868@gmail.com>
2026-02-18 17:30:10 -0500

    Created files for each page that will be displayed

---
+++ resources/views/a380.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Airbus a380</title>
+</head>
+<body>
+    <h1> Airbus a380</h1>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,28 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
+
+Route::get('/a380', function () {
+    return view('a380');
+});
+
+Route::get('/a320', function () {
+    return view('a320');
+});
+
+Route::get('/a350', function () {
+    return view('a350');
+});
+
+Route::get('/b747', function () {
+    return view('b747');
+});
+
+Route::get('/b777', function () {
+    return view('b777');
+});
+
+
+
